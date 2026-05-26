fed0f29a3cb659815af79ebdbb85b5721d3f2f1c
Christina Arn <BAA6661>
2026-05-26 07:57:49 +0200

    Erste Version der Funktion get_metadata

--- R/get_metadata.R
+++ R/get_metadata.R
@@ -17,26 +17,46 @@
 #' @export
 
 get_var_meta <- function(var, field = NULL) {
-    meta <- yaml::read_yaml(system.file("qm_sekII_metadata.yaml", package = "metaSABSEB"))
+  meta <- yaml::read_yaml(system.file("qm_sekII_metadata.yaml",
+                                      package = "metaSABSEB"))
 
-    # Check if variable is stored in metadata
-    if(!var %in% names(meta)) {
-        close_match <- names(meta)[tolower(names(meta)) == tolower(var)]
-        if (length(close_match)>0) {
-            stop("Variable '", var, "' konnte nicht in den Metadaten gefunden werden. Meinten Sie vielleicht '", close_match[1], "' ?")
+  if (!var %in% names(meta)) {
+    close_match <- names(meta)[tolower(names(meta)) == tolower(var)]
+
+    if (length(close_match) > 0) {
+      if (interactive()) {
+        choice <- utils::menu(
+          choices = c("Ja", "Nein"),
+          title = paste0("Variable '", var,
+                         "' nicht gefunden. Meinten Sie vielleicht '",
+                         close_match[1], "'?")
+        )
+        if (choice == 1) {
+          var <- close_match[1]
         } else {
-            stop("Variable '", var, "' konnte in den Metadaten nicht gefunden werden")
+          stop("Variable '", var,
+               "' konnte nicht in den Metadaten gefunden werden.")
         }
-    }
-
-    info <- meta[[var]]
-
-    if (!is.null(field)) {
-        if (!field %in% names(info)) {
-            stop("Field '", field, "' not found for variable '", var, "'.")
-        }
-        info[[field]]
+      } else {
+        stop(
+          "Variable '", var,
+          "' konnte nicht in den Metadaten gefunden werden. ",
+          "Meinten Sie vielleicht '",
+          close_match[1], "' ?"
+        )
+      }
     } else {
-        info
+      stop("Variable '", var, "' konnte in den Metadaten nicht gefunden werden")
     }
+  }
+
+  info <- meta[[var]]
+  if (!is.null(field)) {
+    if (!field %in% names(info)) {
+      stop("Field '", field, "' not found for variable '", var, "'.")
+    }
+    info[[field]]
+  } else {
+    info
+  }
 }
